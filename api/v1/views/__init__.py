e73bc53c0596b162b7672d73a14d6f80ac74618c
PeteJames21 <peetjames21@gmail.com>
2024-01-07 00:07:38 +0300

    Add wildcard import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 '''creates a blueprint instance with prefix /app/v1'''
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if app_views is not None:
     from api.v1.views.index import *
