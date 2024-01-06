55e74855acb8695c2ed8771ea486da4d5c28b223
Gabogogi <bencitymaina@gmail.com>
2024-01-06 23:44:32 +0300

    task 4 and 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,7 +19,6 @@ classes = {"users": "User", "places": "Place", "states": "State",
 def status():
     ''' routes to status page '''
     return jsonify({'status': 'OK'})
-<<<<<<< HEAD
 
 
 @app_views.route('/stats', methods=['GET'])
@@ -29,5 +28,3 @@ def count():
     for cls in classes:
         count_dict[cls] = storage.count(classes[cls])
     return jsonify(count_dict)
-=======
->>>>>>> 25c08795b2e2107660250cf47a01be283619ac05
