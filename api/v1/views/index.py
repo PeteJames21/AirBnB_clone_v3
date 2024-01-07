17aea67c426bb57b94858babcfecab0db8569000
PeteJames21 <peetjames21@gmail.com>
2024-01-08 00:05:26 +0300

    Remove unused imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 classes = {"users": "User", "places": "Place", "states": "State",
            "cities": "City", "amenities": "Amenity",
