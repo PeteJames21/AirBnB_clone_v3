cb96e119953e7052e3e1f749db5342be2acd41f2
PeteJames21 <peetjames21@gmail.com>
2024-01-08 00:19:56 +0300

    Refactor imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-'''creates a blueprint instance with prefix /app/v1'''
+"""Creates a blueprint instance with prefix /app/v1."""
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.users import *
+
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
+    from api.v1.views.users import *
